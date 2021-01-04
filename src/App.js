608a04ae74360cfbf4265adff3e9d1fb9106c9df
Javiuty <javiiescuadra@gmail.com>
2021-01-04 19:14:02 +0100

    styling + calling api lyrics

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Formulario from "./components/Formulario";
+
+import axios from "axios";
+import Cancion from "./components/Cancion";
 
 const App = () => {
   const [busquedaletra, guardarBusquedaLetra] = useState({});
+  const [letra, guardarLetra] = useState("");
+
+  useEffect(() => {
+    if (Object.keys(busquedaletra).length === 0) return;
+
+    const consultarApiLetra = async () => {
+      const { artista, cancion } = busquedaletra;
+
+      const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
+
+      const resultado = await axios(url);
+
+      guardarLetra(resultado.data.lyrics);
+    };
+
+    consultarApiLetra();
+  }, [busquedaletra]);
 
   return (
     <>
       <Formulario guardarBusquedaLetra={guardarBusquedaLetra} />
+
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6"></div>
+          <div className="col-md-6">
+            <Cancion letra={letra} />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
